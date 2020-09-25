7eb49291a138b97252333b722263d3ec3b898607
buigabor <bui.gabor97@gmail.com>
2020-09-25 20:02:30 +0200

    Add CollectionView methods

--- src/Views/Collectionview.ts
+++ src/Views/Collectionview.ts
@@ -1,8 +1,9 @@
-import { User, UserProps } from './../Models/User';
 import { Collection } from './../Models/Collection';
 
 export abstract class CollectionView<T, K> {
   constructor(public parent: Element, public collection: Collection<T, K>) {}
+
+  abstract renderItem(model: T, itemParent: Element): void {}
 
   render(): void {}
 }
